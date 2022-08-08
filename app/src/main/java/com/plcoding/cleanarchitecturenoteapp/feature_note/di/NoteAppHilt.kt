9b1e7db0e819b1a08bf8c13b5e84c0f21e2a9135
LAWAL <haydelawal@gmail.com>
2022-08-08 22:49:06 +0100

    Clean Arc Note App JPC

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/di/NoteAppHilt.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+
+@HiltAndroidApp
+class NoteAppHilt: Application(){
+}
